dff8ee5bf8f09a0fd8e97405b62cc77e0fdf9903
DAVINA SINGH <davina_singh@outlook.com>
2024-03-25 13:11:17 +0000

    davina week1

---
+++ demos/week-01/js-audio-video/app.js
@@ -0,0 +1,29 @@
+const nature = document.getElementById("nature");
+const playBtn = document.getElementById("playBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+const stopBtn = document.getElementById("stopBtn");
+const skipBack = document.getElementById("skipBack");
+const skipFor = document.getElementById("skipFor");
+
+// console.dir(nature);
+
+playBtn.addEventListener("click", function () {
+  nature.play();
+});
+
+pauseBtn.addEventListener("click", function () {
+  nature.pause();
+});
+
+stopBtn.addEventListener("click", function () {
+  nature.pause();
+  nature.currentTime = 0;
+});
+
+skipBack.addEventListener("click", function () {
+  nature.currentTime = nature.currentTime - 10;
+});
+
+skipFor.addEventListener("click", function () {
+  nature.currentTime = nature.currentTime + 10;
+});
