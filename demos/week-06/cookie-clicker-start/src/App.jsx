e7794f2f2ae69acbae2560f723748c5830e3e1ec
Tim <tim.smith2814@gmail.com>
2024-04-25 09:51:05 +0100

    Add start of cookie clicker

---
+++ demos/week-06/cookie-clicker-start/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  // what if I want to do local storage? STRETCH GOAL
+  const [cookies, setCookies] = useState(0);
+  const [cps, setCps] = useState(1); // CPS = Cookies Per Second
+
+  useEffect(() => {
+    // maybe you want to do some maths here for the 1000/cps etc
+    // a timer to be created when the page loads to increase cookies by cps every second
+    const myInterval = setInterval(() => {
+      addCookie();
+    }, 1000 / cps);
+
+    // to clean up my timer when I rerun the useEffect to i don't end up with a billion timers
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [cps]);
+
+  function addCookie() {
+    // because this runs in a timer, we need to be more explicit about the previous value of the state variable
+    setCookies((currentCookies) => {
+      // what if I want to do local storage? STRETCH GOAL
+      return currentCookies + 1;
+    });
+  }
+
+  function buyUpgrade() {
+    setCps(cps + 1);
+  }
+
+  return (
+    <div>
+      <h1>Cookie Clicker</h1>
+      <button onClick={addCookie}>I am a cookie</button>
+      <button onClick={buyUpgrade}>Buy upgrade</button>
+      <p>I have {cookies} cookies</p>
+      <p>I get {cps} cookies per second</p>
+    </div>
+  );
+}
