83f2fa7e88b26fa8a5f69420c6b3e8c40e7ff41f
Manuel <manugonzalez.tech@gmail.com>
2024-05-08 14:50:49 +0100

    Add image and font demo

---
+++ demos/week-08/coaster-rollers-image-font/src/app/page.js
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import Image from "next/image";
+
+//import image file
+import rollercoaster from "@/../public/rollercoaster.jpg";
+
+import Button from "../components/Button"; //imported from the components folder
+export default function Home() {
+  return (
+    <>
+      <header>
+        <h1>Coaster Rollers</h1>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </nav>
+      </header>
+      {/* example of a React component */}
+      <Button />
+      <main>
+        <section>
+          <Image
+            src={rollercoaster}
+            alt="rollercoaster"
+            width={300}
+            height={200}
+            placeholder="blur"
+          />
+          <article>
+            <p>
+              A roller coaster is a type of amusement ride employing a form of
+              elevated railroad track that carries passengers on a train through
+              tight turns, steep slopes, and other elements designed to produce
+              a thrilling experience.
+            </p>
+          </article>
+        </section>
+        <section>
+          <Image
+            src={rollercoaster}
+            alt="rollercoaster"
+            width={300}
+            height={200}
+            placeholder="blur"
+          />
+          <article>
+            <p>
+              A roller coaster is a type of amusement ride employing a form of
+              elevated railroad track that carries passengers on a train through
+              tight turns, steep slopes, and other elements designed to produce
+              a thrilling experience.
+            </p>
+          </article>
+        </section>
+        <section>
+          <Image
+            src={rollercoaster}
+            alt="rollercoaster"
+            width={300}
+            height={200}
+          />
+          <article>
+            <p>
+              A roller coaster is a type of amusement ride employing a form of
+              elevated railroad track that carries passengers on a train through
+              tight turns, steep slopes, and other elements designed to produce
+              a thrilling experience.
+            </p>
+          </article>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; Coaster Rollers</p>
+      </footer>
+    </>
+  );
+}
---
+++ demos/week-08/coaster-rollers-image-font/src/components/Button.jsx
@@ -0,0 +1,3 @@
+export default function Button() {
+  return <button className="bg-red-500">Button</button>;
+}
